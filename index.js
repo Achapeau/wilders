419a7724219936f3c0ed86a87647701a5987a874
CHAPEAU Adrien <ad.chap.13.17@gmail.com>
2023-05-19 14:23:54 +0200

    first add

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.bgCyanBright.red("Hello AnnieC!"));
+console.log(chalk.bgYellowBright.black("Hello Sam[Team CHOCO]:"));
+console.log(chalk.bgBlue.white("Hello Cedric[Team Pain au chocolat]!"));
+console.log(chalk.bgRed.white("Hello Tristan[Team Canelés]!"));
